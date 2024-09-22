3422595095c2e2487c2cbbf3c12ee9d7f108a69f
avtandilzahiddin <avtandilabildin2@gmail>
2024-09-22 20:26:05 +0600

    .

---
+++ HW3/sin.f95
@@ -0,0 +1,18 @@
+program sinus
+    implicit none
+    integer, parameter :: n = 1000
+    double precision :: l1, l2, delta, sum, x
+    integer :: i
+
+    l1 = 3.14159265358979323846    ! π
+    l2 = 1.5 * l1                  ! 3π/2
+    delta = (l2 - l1) / n
+    sum = 0.0
+
+    do i = 0, n-1
+        x = l1 + (i + 0.5) * delta
+        sum = sum + sin(x) * delta
+    end do
+
+    write(*,*) 'integral of sin(x) from pi to 3pi/2 is =', sum
+end program sinus
